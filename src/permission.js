c46418f0ad66e96ecdf815a85a478cd9884f2050
LiYuze <228487099@qq.com>
2022-11-15 11:59:55 +0800

    update:微调判断规则

--- src/permission.js
+++ src/permission.js
@@ -1,17 +1,19 @@
 import router from './router/index'
 import store from './store/index'
 
-router.beforeEach((to, from, next)=>{
-  if(to.path.includes('/admin')){
-    if(store.state.user.loginIn === true){
+router.beforeEach((to, from, next) => {
+  const {path} = to
+  if (path.indexOf('/admin') === 0) {
+    const loginIn = store.getters.loginIn || sessionStorage.getItem('loginIn')
+    if (loginIn) {
       next()
-    }else{
+    } else {
       alert('请先登录')
       next('/login')
     }
-    
-  }else{
+
+  } else {
     next()
   }
-  
+
 })
